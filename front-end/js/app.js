c0b3119911df8c8488715f1c5c634c57fbaed0c1
fuentesj <jonathan.wilfredo.fuentes@gmail.com>
2016-02-20 23:15:17 -0800

    1. Moved keypair generation HTML to its own partial. 2. Began using ng route service to route URL's to appropriate partials. 3. Added side navbar.

--- front-end/js/app.js
+++ front-end/js/app.js
@@ -1,1 +1,21 @@
-var app = angular.module('EasyKeyzApp', []);
+var app = angular.module('EasyKeyzApp', ['ngRoute']);
+
+app.config(function($routeProvider) {
+
+
+	$routeProvider
+		.when('/keypairGeneration', {
+			templateUrl: '../views/keypairGeneration.html'
+		})
+
+		.when('/csrGeneration', {
+			templateUrl: '../views/csrGeneration.html'
+		})
+
+
+
+
+
+
+
+})

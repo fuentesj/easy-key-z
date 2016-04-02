d910c6e3be53d86c15ae7387d23edd2844340df0
fuentesj <jonathan.wilfredo.fuentes@gmail.com>
2016-04-02 10:26:30 -0700

    1. Added task to install Oracle JDK. 2. Added two-way binding for trusstores variable in trusstore controller.

--- server/static/js/controllers/truststoreConfigurationController.js
+++ server/static/js/controllers/truststoreConfigurationController.js
@@ -1,7 +1,18 @@
 app.controller("TruststoreConfigurationController", ["$scope", "TruststoreConfigurationService", function($scope, TruststoreConfigurationService) {
 
+	$scope.truststores = undefined;
+
 	$scope.$watch('$viewContentLoaded', function() {
 
+		var promise = TruststoreConfigurationService.fetchTruststores();
+		promise.then(
+			function success(response) {
+				$scope.truststores = response.data.results;
+			},
+			function error(error) {
+				console.log("error with loading trustores");
+			}
+		);
 
 	});
 

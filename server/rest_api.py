6a83ae774ca0bbe94701b0a01aa90abeff7bf1eb
fuentesj <jonathan.wilfredo.fuentes@gmail.com>
2016-04-04 07:20:25 -0700

    1. Fixing filepaths for selected truststore and certificate to be imported.

--- server/rest_api.py
+++ server/rest_api.py
@@ -99,12 +99,11 @@ def add_certificate():
 	with open(TRUSTSTORE_DIR + "current-cert.pem", "w") as current_certificate_file:
 		current_certificate_file.write(certificate)
 
-	truststore_file = TRUSTSTORE_DIR + "current-cert.pem"
-
-	selected_truststore = str(request_data['selectedTruststore'])
+	certificate_file_to_be_imported = TRUSTSTORE_DIR + "current-cert.pem"
+	selected_truststore = TRUSTSTORE_DIR + str(request_data['selectedTruststore'])
 	alias = str(request_data['alias'])
 	passphrase = str(request_data['passphrase'])
-	process = subprocess.Popen(['sudo keytool', '-import', '-alias', alias, '-keystore', selected_truststore, '-storepass', passphrase, '-file', truststore_file], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+	process = subprocess.Popen(['sudo keytool', '-import', '-alias', alias, '-keystore', selected_truststore, '-storepass', passphrase, '-file', certificate_file_to_be_imported], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 	result_tuple = process.communicate("yes" + "\n")
 	print result_tuple
 	responseObject = {}

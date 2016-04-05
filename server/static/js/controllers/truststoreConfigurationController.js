bf758c509ffe97b50174b8f7949a8524530c6be2
fuentesj <jonathan.wilfredo.fuentes@gmail.com>
2016-04-05 06:57:11 -0700

    1. Removed required directive from inputs in truststore partial, and added red asterisk instead.

--- server/static/js/controllers/truststoreConfigurationController.js
+++ server/static/js/controllers/truststoreConfigurationController.js
@@ -29,10 +29,10 @@ app.controller("TruststoreConfigurationController", ["$scope", "TruststoreConfig
 
 	$scope.submitCertificate = function() {
 
-		if (!scope.selectedTruststore || !scope.alias || !scope.certificate) {
+		if (!$scope.selectedTruststore || !$scope.alias || !$scope.passphrase || !$scope.certificate) {
 			$scope.errorMessage = "Please fill out all required fields."
 			if ($scope.showSuccessAlert) {
-					$scope.showSuccessAlert = false;
+				$scope.showSuccessAlert = false;
 			}
 			$scope.showErrorAlert = true;
 			return;
